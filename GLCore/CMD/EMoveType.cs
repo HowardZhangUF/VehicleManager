d3803b99102a8c851cb2ea3fc6fff4d930572233
Wood <Administrator@WIN-HAFHBSLTM8J>
2018-04-27 10:46:44 +0800

    重構-移動檔案

---
+++ GLCore/CMD/EMoveType.cs
@@ -0,0 +1,43 @@
+﻿namespace GLCore
+{
+    /// <summary>
+    /// 移動方式
+    /// </summary>
+    public enum EMoveType
+    {
+        /// <summary>
+        /// 不移動
+        /// </summary>
+        Stop,
+
+        /// <summary>
+        /// 移動中心點
+        /// </summary>
+        Center,
+
+        /// <summary>
+        /// 移動最大值座標
+        /// </summary>
+        Max,
+
+        /// <summary>
+        /// 移動最小值座標
+        /// </summary>
+        Min,
+
+        /// <summary>
+        /// 移動起點座標
+        /// </summary>
+        Begin,
+
+        /// <summary>
+        /// 移動終點座標
+        /// </summary>
+        End,
+
+        /// <summary>
+        /// 移動方向角
+        /// </summary>
+        Toward,
+    }
+}
---
+++ GLCore/CMD/SerialNumberManager.cs
@@ -0,0 +1,30 @@
+﻿namespace GLCore
+{
+    /// <summary>
+    /// 遞增序號管理器
+    /// </summary>
+    public class SerialNumberManager
+    {
+        /// <summary>
+        /// 執行緒鎖
+        /// </summary>
+        private object key = new object();
+
+        /// <summary>
+        /// 編號
+        /// </summary>
+        private int number = 0;
+
+        /// <summary>
+        /// 取得下一個不重複的號碼
+        /// </summary>
+        public int Next()
+        {
+            lock (key)
+            {
+                number++;
+                return number;
+            }
+        }
+    }
+}
