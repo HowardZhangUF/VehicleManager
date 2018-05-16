85a283b27ec6ba8d58303150231b6f0cd9a27558
Wood <Administrator@WIN-HAFHBSLTM8J>
2018-05-16 15:53:51 +0800

    重構-移動類別

---
+++ GLUI/Events.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLUI
+{
+    #region 地圖載入
+
+    /// <summary>
+    /// 地圖載入事件委派
+    /// </summary>
+    public delegate void LoadMapEvent(object sender, LoadMapEventArgs e);
+
+    /// <summary>
+    /// 地圖載入事件參數
+    /// </summary>
+    public class LoadMapEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 地圖路徑
+        /// </summary>
+        public string MapPath { get; set; }
+    }
+
+    #endregion 地圖載入
+}
--- GLUI/GLUICtrl.cs
+++ GLUI/GLUICtrl.cs
@@ -920,24 +920,4 @@ namespace GLUI
             return new Pair(mX * Zoom - Translate.X, mY * Zoom - Translate.Y);
         }
     }
-
-    #region 地圖載入
-
-    /// <summary>
-    /// 地圖載入事件委派
-    /// </summary>
-    public delegate void LoadMapEvent(object sender, LoadMapEventArgs e);
-
-    /// <summary>
-    /// 地圖載入事件參數
-    /// </summary>
-    public class LoadMapEventArgs : EventArgs
-    {
-        /// <summary>
-        /// 地圖路徑
-        /// </summary>
-        public string MapPath { get; set; }
-    }
-
-    #endregion 地圖載入
 }
