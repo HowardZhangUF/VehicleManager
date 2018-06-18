ac430be0774e207204bf7d7cd4a83dc3f9dcdff3
jauhueichen <jauhueichen@gmail.com>
2018-06-18 16:09:45 +0800

    重構-搬移類別

--- Serialization/SerialServer.cs
+++ Serialization/SerialServer.cs
@@ -6,36 +6,6 @@ using static Serialization.StandardOperate;
 
 namespace Serialization
 {
-    #region 接收序列化資料
-
-    /// <summary>
-    /// 接收序列化資料委派
-    /// </summary>
-    public delegate void ReceivedSerialDataEvent(object sender, ReceivedSerialDataEventArgs e);
-
-    /// <summary>
-    /// 接收序列化資料事件參數
-    /// </summary>
-    public class ReceivedSerialDataEventArgs : EventArgs
-    {
-        /// <summary>
-        /// 接收到的序列化資料
-        /// </summary>
-        public ISerializable Data { get; set; }
-
-        /// <summary>
-        /// 接收時間
-        /// </summary>
-        public DateTime ReceivedTime { get; set; }
-
-        /// <summary>
-        /// 遠端資訊，有可能是 Server 端也有可能是 Client 端。
-        /// </summary>
-        public EndPointInfo RemoteInfo { get; set; }
-    }
-
-    #endregion 接收序列化資料
-
     /// <summary>
     /// 非同步通訊伺服端
     /// </summary>
---
+++ Serialization/StringMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Serialization
+{
+    /// <summary>
+    /// 可支援序列化通訊的字串訊息
+    /// </summary>
+    [Serializable]
+    public class StringMessage : ISerializable
+    {
+        /// <summary>
+        /// 訊息內容
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 訊息時間戳
+        /// </summary>
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// 流水號，用來識別遠端訊息回應的對象
+        /// </summary>
+        public uint TxID { get; set; }
+    }
+}
