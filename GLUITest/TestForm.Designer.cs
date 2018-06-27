a0d52e17e0c7336ec53b5ac81f4e7dae326ca74f
frank <frankpan@castec.com.tw>
2018-06-27 15:29:39 +0800

    VM 介面新增 AGV 狀態監控、調整

--- GLUITest/TestForm.Designer.cs
+++ GLUITest/TestForm.Designer.cs
@@ -33,8 +33,17 @@
 			this.GLUI = new GLUI.GLUICtrl();
 			this.tsslConnectStatus = new System.Windows.Forms.ToolStripStatusLabel();
 			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.cbAGVList = new System.Windows.Forms.ComboBox();
+			this.dgvAGVInfo = new System.Windows.Forms.DataGridView();
 			((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
 			this.statusStrip1.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvAGVInfo)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// dgvInfo
@@ -42,11 +51,11 @@
 			this.dgvInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
 			this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgvInfo.Location = new System.Drawing.Point(16, 48);
+			this.dgvInfo.Location = new System.Drawing.Point(7, 38);
 			this.dgvInfo.Margin = new System.Windows.Forms.Padding(4);
 			this.dgvInfo.Name = "dgvInfo";
 			this.dgvInfo.RowTemplate.Height = 24;
-			this.dgvInfo.Size = new System.Drawing.Size(669, 519);
+			this.dgvInfo.Size = new System.Drawing.Size(650, 475);
 			this.dgvInfo.TabIndex = 1;
 			this.dgvInfo.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvInfo_CellValueChanged);
 			this.dgvInfo.DoubleClick += new System.EventHandler(this.DgvInfo_DoubleClick);
@@ -54,10 +63,10 @@
 			// cmbSelectType
 			// 
 			this.cmbSelectType.FormattingEnabled = true;
-			this.cmbSelectType.Location = new System.Drawing.Point(16, 15);
+			this.cmbSelectType.Location = new System.Drawing.Point(7, 7);
 			this.cmbSelectType.Margin = new System.Windows.Forms.Padding(4);
 			this.cmbSelectType.Name = "cmbSelectType";
-			this.cmbSelectType.Size = new System.Drawing.Size(668, 23);
+			this.cmbSelectType.Size = new System.Drawing.Size(650, 23);
 			this.cmbSelectType.TabIndex = 2;
 			this.cmbSelectType.SelectedValueChanged += new System.EventHandler(this.CmbSelectType_SelectedValueChanged);
 			// 
@@ -96,14 +105,70 @@
 			this.statusStrip1.TabIndex = 4;
 			this.statusStrip1.Text = "statusStrip1";
 			// 
+			// tabControl1
+			// 
+			this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.ItemSize = new System.Drawing.Size(40, 40);
+			this.tabControl1.Location = new System.Drawing.Point(12, 12);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(672, 568);
+			this.tabControl1.TabIndex = 5;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.dgvAGVInfo);
+			this.tabPage1.Controls.Add(this.cbAGVList);
+			this.tabPage1.Location = new System.Drawing.Point(4, 44);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(664, 520);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "AGVs";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.dgvInfo);
+			this.tabPage2.Controls.Add(this.cmbSelectType);
+			this.tabPage2.Location = new System.Drawing.Point(4, 44);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(664, 520);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Objects";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// cbAGVList
+			// 
+			this.cbAGVList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbAGVList.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.cbAGVList.FormattingEnabled = true;
+			this.cbAGVList.Location = new System.Drawing.Point(6, 6);
+			this.cbAGVList.Name = "cbAGVList";
+			this.cbAGVList.Size = new System.Drawing.Size(303, 38);
+			this.cbAGVList.TabIndex = 0;
+			// 
+			// dgvAGVInfo
+			// 
+			this.dgvAGVInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvAGVInfo.Location = new System.Drawing.Point(6, 50);
+			this.dgvAGVInfo.Name = "dgvAGVInfo";
+			this.dgvAGVInfo.RowTemplate.Height = 27;
+			this.dgvAGVInfo.Size = new System.Drawing.Size(303, 311);
+			this.dgvAGVInfo.TabIndex = 1;
+			// 
 			// frmTest
 			// 
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
 			this.ClientSize = new System.Drawing.Size(1368, 608);
+			this.Controls.Add(this.tabControl1);
 			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.GLUI);
-			this.Controls.Add(this.cmbSelectType);
-			this.Controls.Add(this.dgvInfo);
 			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "frmTest";
 			this.Text = "GLUITest";
@@ -111,6 +176,10 @@
 			((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
 			this.statusStrip1.ResumeLayout(false);
 			this.statusStrip1.PerformLayout();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dgvAGVInfo)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -122,6 +191,11 @@
         private GLUI.GLUICtrl GLUI;
 		private System.Windows.Forms.ToolStripStatusLabel tsslConnectStatus;
 		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.DataGridView dgvAGVInfo;
+		private System.Windows.Forms.ComboBox cbAGVList;
 	}
 }
 
