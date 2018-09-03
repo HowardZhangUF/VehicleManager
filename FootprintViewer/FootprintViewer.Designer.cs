f1e97bc3bc33d1ab76410153360b0ffc09543395
frank <frankpan@castec.com.tw>
2018-09-03 20:31:47 +0800

    新增 FootprintViewer 專案

---
+++ FootprintViewer/FootprintViewer.Designer.cs
@@ -0,0 +1,457 @@
+﻿namespace FootprintViewer
+{
+	partial class FootprintViewer
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cbYear1 = new System.Windows.Forms.ComboBox();
+			this.cbMonth1 = new System.Windows.Forms.ComboBox();
+			this.cbDay1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.cbHour1 = new System.Windows.Forms.ComboBox();
+			this.cbMinute1 = new System.Windows.Forms.ComboBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.cbSecond1 = new System.Windows.Forms.ComboBox();
+			this.cbSecond2 = new System.Windows.Forms.ComboBox();
+			this.cbMinute2 = new System.Windows.Forms.ComboBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.cbHour2 = new System.Windows.Forms.ComboBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.cbDay2 = new System.Windows.Forms.ComboBox();
+			this.cbMonth2 = new System.Windows.Forms.ComboBox();
+			this.cbYear2 = new System.Windows.Forms.ComboBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.txtFootprintDirectory = new System.Windows.Forms.TextBox();
+			this.btnBrowseFootprintDirectory = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label12 = new System.Windows.Forms.Label();
+			this.btnBrowseMapPath = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label13 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.btnSetTimeInterval = new System.Windows.Forms.Button();
+			this.gluiCtrl1 = new GLUI.GLUICtrl();
+			this.SuspendLayout();
+			// 
+			// cbYear1
+			// 
+			this.cbYear1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbYear1.FormattingEnabled = true;
+			this.cbYear1.Location = new System.Drawing.Point(144, 97);
+			this.cbYear1.Name = "cbYear1";
+			this.cbYear1.Size = new System.Drawing.Size(65, 23);
+			this.cbYear1.TabIndex = 16;
+			// 
+			// cbMonth1
+			// 
+			this.cbMonth1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbMonth1.FormattingEnabled = true;
+			this.cbMonth1.Location = new System.Drawing.Point(232, 97);
+			this.cbMonth1.Name = "cbMonth1";
+			this.cbMonth1.Size = new System.Drawing.Size(60, 23);
+			this.cbMonth1.TabIndex = 17;
+			// 
+			// cbDay1
+			// 
+			this.cbDay1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbDay1.FormattingEnabled = true;
+			this.cbDay1.Location = new System.Drawing.Point(315, 97);
+			this.cbDay1.Name = "cbDay1";
+			this.cbDay1.Size = new System.Drawing.Size(60, 23);
+			this.cbDay1.TabIndex = 18;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(215, 100);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(11, 15);
+			this.label1.TabIndex = 19;
+			this.label1.Text = "/";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(298, 100);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(11, 15);
+			this.label2.TabIndex = 20;
+			this.label2.Text = "/";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(472, 100);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(11, 15);
+			this.label3.TabIndex = 21;
+			this.label3.Text = ":";
+			// 
+			// cbHour1
+			// 
+			this.cbHour1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbHour1.FormattingEnabled = true;
+			this.cbHour1.Location = new System.Drawing.Point(406, 97);
+			this.cbHour1.Name = "cbHour1";
+			this.cbHour1.Size = new System.Drawing.Size(60, 23);
+			this.cbHour1.TabIndex = 22;
+			// 
+			// cbMinute1
+			// 
+			this.cbMinute1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbMinute1.FormattingEnabled = true;
+			this.cbMinute1.Location = new System.Drawing.Point(489, 97);
+			this.cbMinute1.Name = "cbMinute1";
+			this.cbMinute1.Size = new System.Drawing.Size(60, 23);
+			this.cbMinute1.TabIndex = 24;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(555, 100);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(11, 15);
+			this.label4.TabIndex = 23;
+			this.label4.Text = ":";
+			// 
+			// cbSecond1
+			// 
+			this.cbSecond1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbSecond1.FormattingEnabled = true;
+			this.cbSecond1.Location = new System.Drawing.Point(572, 97);
+			this.cbSecond1.Name = "cbSecond1";
+			this.cbSecond1.Size = new System.Drawing.Size(60, 23);
+			this.cbSecond1.TabIndex = 25;
+			// 
+			// cbSecond2
+			// 
+			this.cbSecond2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbSecond2.FormattingEnabled = true;
+			this.cbSecond2.Location = new System.Drawing.Point(572, 127);
+			this.cbSecond2.Name = "cbSecond2";
+			this.cbSecond2.Size = new System.Drawing.Size(60, 23);
+			this.cbSecond2.TabIndex = 35;
+			// 
+			// cbMinute2
+			// 
+			this.cbMinute2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbMinute2.FormattingEnabled = true;
+			this.cbMinute2.Location = new System.Drawing.Point(489, 127);
+			this.cbMinute2.Name = "cbMinute2";
+			this.cbMinute2.Size = new System.Drawing.Size(60, 23);
+			this.cbMinute2.TabIndex = 34;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(555, 130);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(11, 15);
+			this.label5.TabIndex = 33;
+			this.label5.Text = ":";
+			// 
+			// cbHour2
+			// 
+			this.cbHour2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbHour2.FormattingEnabled = true;
+			this.cbHour2.Location = new System.Drawing.Point(406, 127);
+			this.cbHour2.Name = "cbHour2";
+			this.cbHour2.Size = new System.Drawing.Size(60, 23);
+			this.cbHour2.TabIndex = 32;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(472, 130);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(11, 15);
+			this.label6.TabIndex = 31;
+			this.label6.Text = ":";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(298, 130);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(11, 15);
+			this.label7.TabIndex = 30;
+			this.label7.Text = "/";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(215, 130);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(11, 15);
+			this.label8.TabIndex = 29;
+			this.label8.Text = "/";
+			// 
+			// cbDay2
+			// 
+			this.cbDay2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbDay2.FormattingEnabled = true;
+			this.cbDay2.Location = new System.Drawing.Point(315, 127);
+			this.cbDay2.Name = "cbDay2";
+			this.cbDay2.Size = new System.Drawing.Size(60, 23);
+			this.cbDay2.TabIndex = 28;
+			// 
+			// cbMonth2
+			// 
+			this.cbMonth2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbMonth2.FormattingEnabled = true;
+			this.cbMonth2.Location = new System.Drawing.Point(232, 127);
+			this.cbMonth2.Name = "cbMonth2";
+			this.cbMonth2.Size = new System.Drawing.Size(60, 23);
+			this.cbMonth2.TabIndex = 27;
+			// 
+			// cbYear2
+			// 
+			this.cbYear2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbYear2.FormattingEnabled = true;
+			this.cbYear2.Location = new System.Drawing.Point(144, 127);
+			this.cbYear2.Name = "cbYear2";
+			this.cbYear2.Size = new System.Drawing.Size(65, 23);
+			this.cbYear2.TabIndex = 26;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(90, 100);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(48, 15);
+			this.label9.TabIndex = 36;
+			this.label9.Text = "Time1:";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(90, 130);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(48, 15);
+			this.label10.TabIndex = 37;
+			this.label10.Text = "Time2:";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(90, 67);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(122, 15);
+			this.label11.TabIndex = 38;
+			this.label11.Text = "Footprint Directory:";
+			// 
+			// txtFootprintDirectory
+			// 
+			this.txtFootprintDirectory.Location = new System.Drawing.Point(218, 64);
+			this.txtFootprintDirectory.Name = "txtFootprintDirectory";
+			this.txtFootprintDirectory.Size = new System.Drawing.Size(552, 25);
+			this.txtFootprintDirectory.TabIndex = 39;
+			// 
+			// btnBrowseFootprintDirectory
+			// 
+			this.btnBrowseFootprintDirectory.Location = new System.Drawing.Point(776, 63);
+			this.btnBrowseFootprintDirectory.Name = "btnBrowseFootprintDirectory";
+			this.btnBrowseFootprintDirectory.Size = new System.Drawing.Size(75, 30);
+			this.btnBrowseFootprintDirectory.TabIndex = 40;
+			this.btnBrowseFootprintDirectory.Text = "...";
+			this.btnBrowseFootprintDirectory.UseVisualStyleBackColor = true;
+			this.btnBrowseFootprintDirectory.Click += new System.EventHandler(this.btnBrowseFootprintDirectory_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(820, 564);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 30);
+			this.button1.TabIndex = 44;
+			this.button1.Text = "...";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(262, 565);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(552, 25);
+			this.textBox1.TabIndex = 43;
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(90, 568);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(166, 15);
+			this.label12.TabIndex = 42;
+			this.label12.Text = "Inspection Result Directory:";
+			// 
+			// btnBrowseMapPath
+			// 
+			this.btnBrowseMapPath.Location = new System.Drawing.Point(776, 27);
+			this.btnBrowseMapPath.Name = "btnBrowseMapPath";
+			this.btnBrowseMapPath.Size = new System.Drawing.Size(75, 30);
+			this.btnBrowseMapPath.TabIndex = 47;
+			this.btnBrowseMapPath.Text = "...";
+			this.btnBrowseMapPath.UseVisualStyleBackColor = true;
+			this.btnBrowseMapPath.Click += new System.EventHandler(this.btnBrowseMapPath_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(218, 28);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(552, 25);
+			this.textBox2.TabIndex = 46;
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(90, 31);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(66, 15);
+			this.label13.TabIndex = 45;
+			this.label13.Text = "Map Path:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(93, 609);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(802, 23);
+			this.comboBox1.TabIndex = 48;
+			// 
+			// btnSetTimeInterval
+			// 
+			this.btnSetTimeInterval.Location = new System.Drawing.Point(671, 96);
+			this.btnSetTimeInterval.Name = "btnSetTimeInterval";
+			this.btnSetTimeInterval.Size = new System.Drawing.Size(99, 55);
+			this.btnSetTimeInterval.TabIndex = 49;
+			this.btnSetTimeInterval.Text = "Set";
+			this.btnSetTimeInterval.UseVisualStyleBackColor = true;
+			// 
+			// gluiCtrl1
+			// 
+			this.gluiCtrl1.AllowObjectMenu = true;
+			this.gluiCtrl1.AllowUndoMenu = true;
+			this.gluiCtrl1.Location = new System.Drawing.Point(286, 213);
+			this.gluiCtrl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.gluiCtrl1.Name = "gluiCtrl1";
+			this.gluiCtrl1.ShowAxis = true;
+			this.gluiCtrl1.ShowGrid = true;
+			this.gluiCtrl1.Size = new System.Drawing.Size(263, 214);
+			this.gluiCtrl1.TabIndex = 50;
+			this.gluiCtrl1.Zoom = 10D;
+			// 
+			// FootprintViewer
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(1195, 654);
+			this.Controls.Add(this.gluiCtrl1);
+			this.Controls.Add(this.btnSetTimeInterval);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.btnBrowseMapPath);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.btnBrowseFootprintDirectory);
+			this.Controls.Add(this.txtFootprintDirectory);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.cbSecond2);
+			this.Controls.Add(this.cbMinute2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.cbHour2);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.cbDay2);
+			this.Controls.Add(this.cbMonth2);
+			this.Controls.Add(this.cbYear2);
+			this.Controls.Add(this.cbSecond1);
+			this.Controls.Add(this.cbMinute1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.cbHour1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.cbDay1);
+			this.Controls.Add(this.cbMonth1);
+			this.Controls.Add(this.cbYear1);
+			this.Name = "FootprintViewer";
+			this.Text = "Footprint Viewer";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FootprintViewer_FormClosing);
+			this.Load += new System.EventHandler(this.FootprintViewer_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ComboBox cbYear1;
+		private System.Windows.Forms.ComboBox cbMonth1;
+		private System.Windows.Forms.ComboBox cbDay1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox cbHour1;
+		private System.Windows.Forms.ComboBox cbMinute1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox cbSecond1;
+		private System.Windows.Forms.ComboBox cbSecond2;
+		private System.Windows.Forms.ComboBox cbMinute2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ComboBox cbHour2;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.ComboBox cbDay2;
+		private System.Windows.Forms.ComboBox cbMonth2;
+		private System.Windows.Forms.ComboBox cbYear2;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox txtFootprintDirectory;
+		private System.Windows.Forms.Button btnBrowseFootprintDirectory;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Button btnBrowseMapPath;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button btnSetTimeInterval;
+		private GLUI.GLUICtrl gluiCtrl1;
+	}
+}
+
---
+++ FootprintViewer/FootprintViewer.cs
@@ -0,0 +1,141 @@
+﻿using GLStyle;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FootprintViewer
+{
+	public partial class FootprintViewer : Form
+	{
+		public FootprintViewer()
+		{
+			InitializeComponent();
+		}
+
+		private void FootprintViewer_Load(object sender, EventArgs e)
+		{
+			StyleManager.LoadStyle("Style.ini");
+			gluiCtrl1.SetEditMode(false);
+			gluiCtrl1.SetControlMode(false);
+
+			if (txtFootprintDirectory.Text != "")
+			{
+				checkFootprintPath(txtFootprintDirectory.Text);
+			}
+		}
+
+		private void FootprintViewer_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			
+		}
+
+		private void btnBrowseFootprintDirectory_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+			if (folderBrowserDialog.ShowDialog() != DialogResult.OK) return;
+			checkFootprintPath(folderBrowserDialog.SelectedPath);
+		}
+
+		/// <summary>
+		/// Footprint 資料夾關鍵字
+		/// </summary>
+		private string FOOTPRINT_DIRECTORY_KEYWORD = "Footprint";
+
+		/// <summary>
+		/// 確認 Footprint 資料夾的時間區間(年)，並更新介面的 ComboBox
+		/// </summary>
+		private void checkFootprintPath(string path)
+		{
+			if (Directory.Exists(path))
+			{
+				DirectoryInfo baseDirInfo = new DirectoryInfo(path);
+				if (baseDirInfo.Name.Contains(FOOTPRINT_DIRECTORY_KEYWORD))
+				{
+					DirectoryInfo[] dirInfos = baseDirInfo.GetDirectories();
+					int yearMin = 0, yearMax = 0;
+					foreach (DirectoryInfo dirInfo in dirInfos)
+					{
+						int num = 0;
+						// format: yyMMdd
+						if (dirInfo.Name.Length == 6 && int.TryParse(dirInfo.Name, out num))
+						{
+							int year = int.Parse(dirInfo.Name.Substring(0, 2)) + DateTime.Now.Year / 100 * 100;
+							if (yearMin == 0 && yearMax == 0)
+							{
+								yearMin = yearMax = year;
+							}
+							else
+							{
+								yearMin = Math.Min(yearMin, year);
+								yearMax = Math.Max(yearMax, year);
+							}
+						}
+					}
+					initializeTimeComboBox(yearMin, yearMax);
+				}
+			}
+		}
+
+		/// <summary>
+		/// 初始化時間區間的 ComboBox
+		/// </summary>
+		private void initializeTimeComboBox(int yearMin, int yearMax)
+		{
+			for (int i = yearMin; i <= yearMax; ++i)
+			{
+				cbYear1.Items.Add(i.ToString());
+				cbYear2.Items.Add(i.ToString());
+			}
+
+			string[] obj1 = new string[12];
+			for (int i = 0; i < obj1.Count(); ++i)
+				obj1[i] = (i + 1).ToString().PadLeft(2, '0');
+			string[] obj2 = new string[31];
+			for (int i = 0; i < obj2.Count(); ++i)
+				obj2[i] = (i + 1).ToString().PadLeft(2, '0');
+			string[] obj3 = new string[24];
+			for (int i = 0; i < obj3.Count(); ++i)
+				obj3[i] = i.ToString().PadLeft(2, '0');
+			string[] obj4 = new string[60];
+			for (int i = 0; i < obj4.Count(); ++i)
+				obj4[i] = i.ToString().PadLeft(2, '0');
+
+			cbMonth1.Items.AddRange(obj1);
+			cbMonth2.Items.AddRange(obj1);
+			cbDay1.Items.AddRange(obj2);
+			cbDay2.Items.AddRange(obj2);
+			cbHour1.Items.AddRange(obj3);
+			cbHour2.Items.AddRange(obj3);
+			cbMinute1.Items.AddRange(obj4);
+			cbMinute2.Items.AddRange(obj4);
+			cbSecond1.Items.AddRange(obj4);
+			cbSecond2.Items.AddRange(obj4);
+
+			if (cbYear1.Items.Count > 0) cbYear1.SelectedIndex = 0;
+			if (cbYear2.Items.Count > 0) cbYear2.SelectedIndex = 0;
+			if (cbMonth1.Items.Count > 0) cbMonth1.SelectedIndex = 0;
+			if (cbMonth2.Items.Count > 0) cbMonth2.SelectedIndex = 0;
+			if (cbDay1.Items.Count > 0) cbDay1.SelectedIndex = 0;
+			if (cbDay2.Items.Count > 0) cbDay2.SelectedIndex = 0;
+			if (cbHour1.Items.Count > 0) cbHour1.SelectedIndex = 0;
+			if (cbHour2.Items.Count > 0) cbHour2.SelectedIndex = 0;
+			if (cbMinute1.Items.Count > 0) cbMinute1.SelectedIndex = 0;
+			if (cbMinute2.Items.Count > 0) cbMinute2.SelectedIndex = 0;
+			if (cbSecond1.Items.Count > 0) cbSecond1.SelectedIndex = 0;
+			if (cbSecond2.Items.Count > 0) cbSecond2.SelectedIndex = 0;
+
+		}
+
+		private void btnBrowseMapPath_Click(object sender, EventArgs e)
+		{
+			gluiCtrl1.LoadMap();
+		}
+	}
+}
