fc8ed69c4cb99b9ac168f2541badf05b706f8d46
frank <frankpan@castec.com.tw>
2018-10-31 10:03:37 +0800

    新增 Vector2D 類別

---
+++ TrafficControlTest/Vector2D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficControlTest
+{
+	/// <summary>二維向量</summary>
+	public class Vector2D
+	{
+		public int XComponent;
+		public int YComponent;
+		public override string ToString()
+		{
+			string tmp = "";
+			tmp += $"Vector: {XComponent}i + {YComponent}j\n";
+			return tmp;
+		}
+
+		/// <summary>計算兩點的二維度的向量</summary>
+		public static Vector2D CalculateVector2D(int x1, int y1, int x2, int y2)
+		{
+			Vector2D result = null;
+			result = new Vector2D();
+			result.XComponent = x2 - x1;
+			result.YComponent = y2 - y1;
+			return result;
+		}
+	}
+}
