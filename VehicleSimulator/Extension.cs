25765364a3f61f960bc86d7657639b19843d9330
frank <frankpan@castec.com.tw>
2018-10-31 16:17:38 +0800

    新增 VehicleSimulator 專案

---
+++ VehicleSimulator/Extension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehicleSimulator
+{
+	public static class Extension
+	{
+		/// <summary>
+		/// 擴充功能，調用控制項，並依照情況調用
+		/// </summary>
+		public static void InvokeIfNecessary<T>(this T ctrl, Action action) where T : Control
+		{
+			if (ctrl.InvokeRequired) { ctrl.Invoke(action); }
+			else { action(); }
+		}
+
+		/// <summary>
+		/// 擴充功能，調用控制項，並依照情況調用
+		/// </summary>
+		public static TResult InvokeIfNecessary<T, TResult>(this T ctrl, Func<T, TResult> action) where T : Control
+		{
+			if (ctrl.InvokeRequired) { return (TResult)ctrl.Invoke(action, ctrl); }
+			else { return action(ctrl); }
+		}
+	}
+}
---
+++ VehicleSimulator/VehicleSimulatorGUI.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace VehicleSimulator
+{
+	partial class VehicleSimulatorGUI
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.gluiCtrl1 = new GLUI.GLUICtrl();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.tabControl1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// gluiCtrl1
+			// 
+			this.gluiCtrl1.AllowObjectMenu = true;
+			this.gluiCtrl1.AllowUndoMenu = true;
+			this.gluiCtrl1.Location = new System.Drawing.Point(13, 28);
+			this.gluiCtrl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.gluiCtrl1.Name = "gluiCtrl1";
+			this.gluiCtrl1.ShowAxis = true;
+			this.gluiCtrl1.ShowGrid = true;
+			this.gluiCtrl1.Size = new System.Drawing.Size(516, 538);
+			this.gluiCtrl1.TabIndex = 0;
+			this.gluiCtrl1.Zoom = 10D;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(536, 27);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.Padding = new System.Drawing.Point(20, 5);
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(444, 540);
+			this.tabControl1.TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Location = new System.Drawing.Point(4, 29);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(436, 507);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 29);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(436, 507);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.statusStrip1.Location = new System.Drawing.Point(0, 570);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(992, 22);
+			this.statusStrip1.TabIndex = 2;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(992, 24);
+			this.menuStrip1.TabIndex = 3;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// VehicleSimulatorGUI
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(992, 592);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.gluiCtrl1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "VehicleSimulatorGUI";
+			this.Text = "Form1";
+			this.tabControl1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private GLUI.GLUICtrl gluiCtrl1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+	}
+}
+
