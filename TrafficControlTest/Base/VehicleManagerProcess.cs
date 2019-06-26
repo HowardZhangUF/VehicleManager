c308acb89ff11d466cfe33d1a8ed5ad177a4f844
frank <frankpan@castec.com.tw>
2019-06-26 11:23:34 +0800

    加入 VehicleManagerProcess 類別與 VehicleManagerGUI 表單

---
+++ TrafficControlTest/Base/VehicleManagerProcess.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Threading.Tasks;
+using TrafficControlTest.Interface;
+using TrafficControlTest.Library;
+using static TrafficControlTest.Library.EventHandlerLibraryOfDebugMessage;
+using static TrafficControlTest.Library.EventHandlerLibraryOfIVehicleCommunicator;
+using static TrafficControlTest.Library.EventHandlerLibraryOfIVehicleInfoManager;
+using static TrafficControlTest.Library.Library;
+
+namespace TrafficControlTest.Base
+{
+	class VehicleManagerProcess
+	{
+		public event EventHandlerDateTime VehicleCommunicatorSystemStarted;
+		public event EventHandlerDateTime VehicleCommunicatorSystemStopped;
+		public event EventHandlerLocalListenState VehicleCommunicatorLocalListenStateChagned;
+		public event EventHandlerRemoteConnectState VehicleCommunicatorRemoteConnectStateChagned;
+		public event EventHandlerSentSerializableData VehicleCommunicatorSentSerializableData;
+		public event EventHandlerReceivedSerializableData VehicleCommunicatorReceivedSerializableData;
+		public event EventHandlerVehicleInfo VehicleInfoManagerVehicleAdded;
+		public event EventHandlerVehicleInfo VehicleInfoManagerVehicleRemoved;
+		public event EventHandlerVehicleInfo VehicleInfoManagerVehicleStateUpdated;
+
+		private IVehicleCommunicator mVehicleCommunicator;
+		private IVehicleInfoManager mVehicleInfoManager;
+
+		public VehicleManagerProcess()
+		{
+			Constructor();
+		}
+		~VehicleManagerProcess()
+		{
+			Destructor();
+		}
+		public void VehicleCommunicatorStartListen(int Port)
+		{
+			mVehicleCommunicator.StartListen(Port);
+		}
+		public void VehicleCommunicatorStopListen()
+		{
+			mVehicleCommunicator.StopListen();
+		}
+
+		private void Constructor()
+		{
+			UnsubscribeEvent_IVehicleCommunicator(mVehicleCommunicator);
+			mVehicleCommunicator = GenerateIVehicleCommunicator();
+			SubscribeEvent_IVehicleCommunicator(mVehicleCommunicator);
+
+			UnsubscribeEvent_IVehicleInfoManager(mVehicleInfoManager);
+			mVehicleInfoManager = GenerateIVehicleInfoManager(mVehicleCommunicator);
+			SubscribeEvent_IVehicleInfoManager(mVehicleInfoManager);
+		}
+		private void Destructor()
+		{
+			UnsubscribeEvent_IVehicleCommunicator(mVehicleCommunicator);
+			UnsubscribeEvent_IVehicleInfoManager(mVehicleInfoManager);
+		}
+		private void SubscribeEvent_IVehicleCommunicator(IVehicleCommunicator VehicleCommunicator)
+		{
+			if (VehicleCommunicator != null)
+			{
+				VehicleCommunicator.SystemStarted += HandleEvent_VehicleCommunicatorSystemStarted;
+				VehicleCommunicator.SystemStopped += HandleEvent_VehicleCommunicatorSystemStopped;
+				VehicleCommunicator.LocalListenStateChanged += HandleEvent_VehicleCommunicatorLocalListenStateChagned;
+				VehicleCommunicator.RemoteConnectStateChanged += HandleEvent_VehicleCommunicatorRemoteConnectStateChagned;
+				VehicleCommunicator.SentSerializableData += HandleEvent_VehicleCommunicatorSentSerializableData;
+				VehicleCommunicator.ReceivedSerializableData += HandleEvent_VehicleCommunicatorReceivedSerializableData;
+			}
+		}
+		private void UnsubscribeEvent_IVehicleCommunicator(IVehicleCommunicator VehicleCommunicator)
+		{
+			if (VehicleCommunicator != null)
+			{
+				VehicleCommunicator.SystemStarted -= HandleEvent_VehicleCommunicatorSystemStarted;
+				VehicleCommunicator.SystemStopped -= HandleEvent_VehicleCommunicatorSystemStopped;
+				VehicleCommunicator.LocalListenStateChanged -= HandleEvent_VehicleCommunicatorLocalListenStateChagned;
+				VehicleCommunicator.RemoteConnectStateChanged -= HandleEvent_VehicleCommunicatorRemoteConnectStateChagned;
+				VehicleCommunicator.SentSerializableData -= HandleEvent_VehicleCommunicatorSentSerializableData;
+				VehicleCommunicator.ReceivedSerializableData -= HandleEvent_VehicleCommunicatorReceivedSerializableData;
+			}
+		}
+		private void SubscribeEvent_IVehicleInfoManager(IVehicleInfoManager VehicleInfoManager)
+		{
+			if (VehicleInfoManager != null)
+			{
+				VehicleInfoManager.VehicleAdded += HandleEvent_VehicleInfoManagerVehicleAdded;
+				VehicleInfoManager.VehicleRemoved += HandleEvent_VehicleInfoManagerVehicleRemoved;
+				VehicleInfoManager.VehicleStateUpdated += HandleEvent_VehicleInfoManagerVehicleStateUpdated;
+			}
+		}
+		private void UnsubscribeEvent_IVehicleInfoManager(IVehicleInfoManager VehicleInfoManager)
+		{
+			if (VehicleInfoManager != null)
+			{
+				VehicleInfoManager.VehicleAdded -= HandleEvent_VehicleInfoManagerVehicleAdded;
+				VehicleInfoManager.VehicleRemoved -= HandleEvent_VehicleInfoManagerVehicleRemoved;
+				VehicleInfoManager.VehicleStateUpdated -= HandleEvent_VehicleInfoManagerVehicleStateUpdated;
+			}
+		}
+		protected virtual void RaiseEvent_VehicleCommunicatorSystemStarted(DateTime OccurTime, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleCommunicatorSystemStarted?.Invoke(OccurTime);
+			}
+			else
+			{
+				Task.Run(() => { VehicleCommunicatorSystemStarted?.Invoke(OccurTime); });
+			}
+		}
+		protected virtual void RaiseEvent_VehicleCommunicatorSystemStopped(DateTime OccurTime, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleCommunicatorSystemStopped?.Invoke(OccurTime);
+			}
+			else
+			{
+				Task.Run(() => { VehicleCommunicatorSystemStopped?.Invoke(OccurTime); });
+			}
+		}
+		protected virtual void RaiseEvent_VehicleCommunicatorLocalListenStateChanged(DateTime OccurTime, ListenState NewState, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleCommunicatorLocalListenStateChagned?.Invoke(OccurTime, NewState);
+			}
+			else
+			{
+				Task.Run(() => { VehicleCommunicatorLocalListenStateChagned?.Invoke(OccurTime, NewState); });
+			}
+		}
+		protected virtual void RaiseEvent_VehicleCommunicatorRemoteConnectStateChagned(DateTime OccurTime, string IpPort, ConnectState NewState, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleCommunicatorRemoteConnectStateChagned?.Invoke(OccurTime, IpPort, NewState);
+			}
+			else
+			{
+				Task.Run(() => { VehicleCommunicatorRemoteConnectStateChagned?.Invoke(OccurTime, IpPort, NewState); });
+			}
+		}
+		protected virtual void RaiseEvent_VehicleCommunicatorSentSerializableData(DateTime OccurTime, string IpPort, object Data, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleCommunicatorSentSerializableData?.Invoke(OccurTime, IpPort, Data);
+			}
+			else
+			{
+				Task.Run(() => { VehicleCommunicatorSentSerializableData?.Invoke(OccurTime, IpPort, Data); });
+			}
+		}
+		protected virtual void RaiseEvent_VehicleCommunicatorReceivedSerializableData(DateTime OccurTime, string IpPort, object Data, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleCommunicatorReceivedSerializableData?.Invoke(OccurTime, IpPort, Data);
+			}
+			else
+			{
+				Task.Run(() => { VehicleCommunicatorReceivedSerializableData?.Invoke(OccurTime, IpPort, Data); });
+			}
+		}
+		protected virtual void RaiseEvent_VehicleInfoManagerVehicleAdded(DateTime OccurTime, string Name, string IpPort, IVehicleInfo VehicleInfo, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleInfoManagerVehicleAdded?.Invoke(OccurTime, Name, IpPort, VehicleInfo);
+			}
+			else
+			{
+				Task.Run(() => { VehicleInfoManagerVehicleAdded?.Invoke(OccurTime, Name, IpPort, VehicleInfo); });
+			}
+		}
+		protected virtual void RaiseEvent_VehicleInfoManagerVehicleRemoved(DateTime OccurTime, string Name, string IpPort, IVehicleInfo VehicleInfo, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleInfoManagerVehicleRemoved?.Invoke(OccurTime, Name, IpPort, VehicleInfo);
+			}
+			else
+			{
+				Task.Run(() => { VehicleInfoManagerVehicleRemoved?.Invoke(OccurTime, Name, IpPort, VehicleInfo); });
+			}
+		}
+		protected virtual void RaiseEvent_VehicleInfoManagerVehicleStateUpdated(DateTime OccurTime, string Name, string IpPort, IVehicleInfo VehicleInfo, bool Sync = true)
+		{
+			if (Sync)
+			{
+				VehicleInfoManagerVehicleStateUpdated?.Invoke(OccurTime, Name, IpPort, VehicleInfo);
+			}
+			else
+			{
+				Task.Run(() => { VehicleInfoManagerVehicleStateUpdated?.Invoke(OccurTime, Name, IpPort, VehicleInfo); });
+			}
+		}
+		private void HandleEvent_VehicleCommunicatorSystemStarted(DateTime OccurTime)
+		{
+			HandleDebugMessage("VehicleCommunicator", "System Started.");
+			RaiseEvent_VehicleCommunicatorSystemStarted(OccurTime);
+		}
+		private void HandleEvent_VehicleCommunicatorSystemStopped(DateTime OccurTime)
+		{
+			HandleDebugMessage("VehicleCommunicator", "System Stopped.");
+			RaiseEvent_VehicleCommunicatorSystemStopped(OccurTime);
+		}
+		private void HandleEvent_VehicleCommunicatorLocalListenStateChagned(DateTime OccurTime, ListenState NewState)
+		{
+			HandleDebugMessage("VehicleCommunicator", $"Local Listen State Changed. State: {NewState.ToString()}");
+			RaiseEvent_VehicleCommunicatorLocalListenStateChanged(OccurTime, NewState);
+		}
+		private void HandleEvent_VehicleCommunicatorRemoteConnectStateChagned(DateTime OccurTime, string IpPort, ConnectState NewState)
+		{
+			HandleDebugMessage("VehicleCommunicator", $"Remote Connect State Changed. IPPort: {IpPort}, State: {NewState}");
+			RaiseEvent_VehicleCommunicatorRemoteConnectStateChagned(OccurTime, IpPort, NewState);
+		}
+		private void HandleEvent_VehicleCommunicatorSentSerializableData(DateTime OccurTime, string IpPort, object Data)
+		{
+			HandleDebugMessage("VehicleCommunicator", $"Sent Serializable Data. IPPort: {IpPort}, DataType: {Data.GetType().ToString()}");
+			RaiseEvent_VehicleCommunicatorSentSerializableData(OccurTime, IpPort, Data);
+		}
+		private void HandleEvent_VehicleCommunicatorReceivedSerializableData(DateTime OccurTime, string IpPort, object Data)
+		{
+			HandleDebugMessage("VehicleCommunicator", $"Received Serializable Data. IPPort: {IpPort}, DataType: {Data.GetType().ToString()}");
+			RaiseEvent_VehicleCommunicatorReceivedSerializableData(OccurTime, IpPort, Data);
+		}
+		private void HandleEvent_VehicleInfoManagerVehicleAdded(DateTime OccurTime, string Name, string IpPort, IVehicleInfo VehicleInfo)
+		{
+			HandleDebugMessage("VehicleInfoManager", $"Vehicle Added. Name: {Name}, IPPort: {IpPort}, Info: {VehicleInfo.ToString()}");
+			RaiseEvent_VehicleInfoManagerVehicleAdded(OccurTime, Name, IpPort, VehicleInfo);
+		}
+		private void HandleEvent_VehicleInfoManagerVehicleRemoved(DateTime OccurTime, string Name, string IpPort, IVehicleInfo VehicleInfo)
+		{
+			HandleDebugMessage("VehicleInfoManager", $"Vehicle Removed. Name: {Name}, IPPort: {IpPort}, Info: {VehicleInfo.ToString()}");
+			RaiseEvent_VehicleInfoManagerVehicleRemoved(OccurTime, Name, IpPort, VehicleInfo);
+		}
+		private void HandleEvent_VehicleInfoManagerVehicleStateUpdated(DateTime OccurTime, string Name, string IpPort, IVehicleInfo VehicleInfo)
+		{
+			HandleDebugMessage("VehicleInfoManager", $"Vehicle State Updated. Name: {Name}, IPPort: {IpPort}, Info: {VehicleInfo.ToString()}");
+			RaiseEvent_VehicleInfoManagerVehicleStateUpdated(OccurTime, Name, IpPort, VehicleInfo);
+		}
+		private void HandleDebugMessage(string Message)
+		{
+			Console.WriteLine(DateTime.Now.ToString(TIME_FORMAT) + " " + Message);
+		}
+		private void HandleDebugMessage(string Category, string Message)
+		{
+			HandleDebugMessage($"[{Category}] - {Message}");
+		}
+	}
+}
---
+++ TrafficControlTest/UserInterface/VehicleManagerGUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TrafficControlTest.Base;
+
+namespace TrafficControlTest.UserInterface
+{
+	public partial class VehicleManagerGUI : Form
+	{
+		VehicleManagerProcess mCore = new VehicleManagerProcess();
+
+		public VehicleManagerGUI()
+		{
+			InitializeComponent();
+
+			try
+			{
+				mCore.VehicleCommunicatorStartListen(8000);
+			}
+			catch (Exception Ex)
+			{
+				HandleException(Ex);
+			}
+		}
+		private void HandleException(Exception Ex)
+		{
+			Console.WriteLine(Ex.ToString());
+		}
+		private void VehicleManagerGUI_Load(object sender, EventArgs e)
+		{
+			try
+			{
+
+			}
+			catch (Exception Ex)
+			{
+				HandleException(Ex);
+			}
+		}
+	}
+}
