b56d27d5610dcc0cf33352a4468fd1bf717d6b4a
frank <frankpan@castec.com.tw>
2019-08-16 17:47:08 +0800

    - VehicleSimulatorGUI 新增連線、移動的功能

--- VehicleSimulator/UserInterface/VehicleSimulatorGUI.Designer.cs
+++ VehicleSimulator/UserInterface/VehicleSimulatorGUI.Designer.cs
@@ -28,21 +28,208 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.BottomToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+			this.TopToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+			this.RightToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+			this.LeftToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+			this.ContentPanel = new System.Windows.Forms.ToolStripContentPanel();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.statusLabelFill = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusLabelHostConnectState = new System.Windows.Forms.ToolStripStatusLabel();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.menuHostConnection = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuHostIpPort = new System.Windows.Forms.ToolStripTextBox();
+			this.menuHostConnect = new System.Windows.Forms.ToolStripMenuItem();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.btnVehicleSimulatorStartMove = new System.Windows.Forms.Button();
+			this.txtVehicleSimulatorPath = new System.Windows.Forms.TextBox();
+			this.btnVehicleSimulatorStopMove = new System.Windows.Forms.Button();
+			this.statusStrip1.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// BottomToolStripPanel
+			// 
+			this.BottomToolStripPanel.Location = new System.Drawing.Point(0, 0);
+			this.BottomToolStripPanel.Name = "BottomToolStripPanel";
+			this.BottomToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.BottomToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.BottomToolStripPanel.Size = new System.Drawing.Size(0, 0);
+			// 
+			// TopToolStripPanel
+			// 
+			this.TopToolStripPanel.Location = new System.Drawing.Point(0, 0);
+			this.TopToolStripPanel.Name = "TopToolStripPanel";
+			this.TopToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.TopToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.TopToolStripPanel.Size = new System.Drawing.Size(0, 0);
+			// 
+			// RightToolStripPanel
+			// 
+			this.RightToolStripPanel.Location = new System.Drawing.Point(0, 0);
+			this.RightToolStripPanel.Name = "RightToolStripPanel";
+			this.RightToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.RightToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.RightToolStripPanel.Size = new System.Drawing.Size(0, 0);
+			// 
+			// LeftToolStripPanel
+			// 
+			this.LeftToolStripPanel.Location = new System.Drawing.Point(0, 0);
+			this.LeftToolStripPanel.Name = "LeftToolStripPanel";
+			this.LeftToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			this.LeftToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+			this.LeftToolStripPanel.Size = new System.Drawing.Size(0, 0);
+			// 
+			// ContentPanel
+			// 
+			this.ContentPanel.Size = new System.Drawing.Size(318, 278);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabelFill,
+            this.statusLabelHostConnectState});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 426);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(800, 24);
+			this.statusStrip1.TabIndex = 0;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// statusLabelFill
+			// 
+			this.statusLabelFill.Name = "statusLabelFill";
+			this.statusLabelFill.Size = new System.Drawing.Size(744, 19);
+			this.statusLabelFill.Spring = true;
+			// 
+			// statusLabelHostConnectState
+			// 
+			this.statusLabelHostConnectState.BackColor = System.Drawing.Color.LightPink;
+			this.statusLabelHostConnectState.Name = "statusLabelHostConnectState";
+			this.statusLabelHostConnectState.Size = new System.Drawing.Size(41, 19);
+			this.statusLabelHostConnectState.Text = "Host";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuHostConnection});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 27);
+			this.menuStrip1.TabIndex = 3;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// menuHostConnection
+			// 
+			this.menuHostConnection.BackColor = System.Drawing.Color.LightPink;
+			this.menuHostConnection.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuHostConnect,
+            this.menuHostIpPort});
+			this.menuHostConnection.Name = "menuHostConnection";
+			this.menuHostConnection.Size = new System.Drawing.Size(136, 23);
+			this.menuHostConnection.Text = "Host Connection";
+			// 
+			// menuHostIpPort
+			// 
+			this.menuHostIpPort.Name = "menuHostIpPort";
+			this.menuHostIpPort.Size = new System.Drawing.Size(100, 27);
+			this.menuHostIpPort.Text = "127.0.0.1:8000";
+			// 
+			// menuHostConnect
+			// 
+			this.menuHostConnect.Name = "menuHostConnect";
+			this.menuHostConnect.Size = new System.Drawing.Size(216, 26);
+			this.menuHostConnect.Text = "Connect";
+			this.menuHostConnect.Click += new System.EventHandler(this.menuHostConnect_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.btnVehicleSimulatorStartMove, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.txtVehicleSimulatorPath, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.btnVehicleSimulatorStopMove, 1, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(407, 84);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(342, 123);
+			this.tableLayoutPanel1.TabIndex = 4;
+			// 
+			// btnVehicleSimulatorStartMove
+			// 
+			this.btnVehicleSimulatorStartMove.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnVehicleSimulatorStartMove.Location = new System.Drawing.Point(203, 3);
+			this.btnVehicleSimulatorStartMove.Name = "btnVehicleSimulatorStartMove";
+			this.btnVehicleSimulatorStartMove.Size = new System.Drawing.Size(65, 117);
+			this.btnVehicleSimulatorStartMove.TabIndex = 2;
+			this.btnVehicleSimulatorStartMove.Text = "Move";
+			this.btnVehicleSimulatorStartMove.UseVisualStyleBackColor = true;
+			this.btnVehicleSimulatorStartMove.Click += new System.EventHandler(this.btnVehicleSimulatorStartMove_Click);
+			// 
+			// txtVehicleSimulatorPath
+			// 
+			this.txtVehicleSimulatorPath.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtVehicleSimulatorPath.Location = new System.Drawing.Point(3, 3);
+			this.txtVehicleSimulatorPath.Multiline = true;
+			this.txtVehicleSimulatorPath.Name = "txtVehicleSimulatorPath";
+			this.txtVehicleSimulatorPath.Size = new System.Drawing.Size(194, 117);
+			this.txtVehicleSimulatorPath.TabIndex = 0;
+			// 
+			// btnVehicleSimulatorStopMove
+			// 
+			this.btnVehicleSimulatorStopMove.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.btnVehicleSimulatorStopMove.Location = new System.Drawing.Point(274, 3);
+			this.btnVehicleSimulatorStopMove.Name = "btnVehicleSimulatorStopMove";
+			this.btnVehicleSimulatorStopMove.Size = new System.Drawing.Size(65, 117);
+			this.btnVehicleSimulatorStopMove.TabIndex = 1;
+			this.btnVehicleSimulatorStopMove.Text = "Stop";
+			this.btnVehicleSimulatorStopMove.UseVisualStyleBackColor = true;
+			this.btnVehicleSimulatorStopMove.Click += new System.EventHandler(this.btnVehicleSimulatorStopMove_Click);
 			// 
 			// VehicleSimulatorGUI
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "VehicleSimulatorGUI";
 			this.Text = "VehicleSimulatorGUI";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VehicleSimulatorGUI_FormClosing);
 			this.Load += new System.EventHandler(this.VehicleSimulatorGUI_Load);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+		private System.Windows.Forms.ToolStripPanel BottomToolStripPanel;
+		private System.Windows.Forms.ToolStripPanel TopToolStripPanel;
+		private System.Windows.Forms.ToolStripPanel RightToolStripPanel;
+		private System.Windows.Forms.ToolStripPanel LeftToolStripPanel;
+		private System.Windows.Forms.ToolStripContentPanel ContentPanel;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel statusLabelFill;
+		private System.Windows.Forms.ToolStripStatusLabel statusLabelHostConnectState;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem menuHostConnection;
+		private System.Windows.Forms.ToolStripTextBox menuHostIpPort;
+		private System.Windows.Forms.ToolStripMenuItem menuHostConnect;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TextBox txtVehicleSimulatorPath;
+		private System.Windows.Forms.Button btnVehicleSimulatorStopMove;
+		private System.Windows.Forms.Button btnVehicleSimulatorStartMove;
 	}
 }
--- VehicleSimulator/UserInterface/VehicleSimulatorGUI.cs
+++ VehicleSimulator/UserInterface/VehicleSimulatorGUI.cs
@@ -9,6 +9,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TrafficControlTest.Interface;
+using TrafficControlTest.Library;
+using VehicleSimulator.Interface;
 
 namespace VehicleSimulator.UserInterface
 {
@@ -30,6 +32,43 @@ namespace VehicleSimulator.UserInterface
 		private void HandleException(Exception Ex)
 		{
 			Console.WriteLine(Ex.ToString());
+		}
+		private bool GetIpPort(string Src, out string Ip, out int Port)
+		{
+			bool result = false;
+			Ip = string.Empty;
+			Port = 0;
+
+			if (Src.Contains(":"))
+			{
+				string[] tmp = Src.Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+				if (tmp.Length == 2)
+				{
+					Ip = tmp[0];
+					Port = int.Parse(tmp[1]);
+					result = true;
+				}
+			}
+			return result;
+		}
+		private bool GetPath(string Src, out IEnumerable<IPoint2D> Path)
+		{
+			bool result = false;
+			Path = null;
+
+			string[] points = Src.Split(new string[] { "\r\n", "(", ")" }, StringSplitOptions.RemoveEmptyEntries);
+			if (points.Length > 0)
+			{
+				List<IPoint2D> tmpPoints = new List<IPoint2D>();
+				foreach (string point in points)
+				{
+					string[] coordinate = point.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+					tmpPoints.Add(TrafficControlTest.Library.Library.GenerateIPoint2D(int.Parse(coordinate[0]), int.Parse(coordinate[1])));
+				}
+				Path = tmpPoints;
+				result = true;
+			}
+			return result;
 		}
 		private void VehicleSimulatorGUI_Load(object sender, EventArgs e)
 		{
@@ -54,32 +93,135 @@ namespace VehicleSimulator.UserInterface
 			}
 		}
 
+		private void UpdateGui_ToolStripItemBackColor(Control ParentContainer, ToolStripItem Control, Color BackColor)
+		{
+			ParentContainer.InvokeIfNecessary(() => Control.BackColor = BackColor);
+		}
+		private void UpdateGui_ControlBackColor(Control Control, Color BackColor)
+		{
+			Control.InvokeIfNecessary((c) => c.BackColor = BackColor);
+		}
+
+		#region VehicleSimulatorProcess
 		private Base.VehicleSimulatorProcess mCore;
+
+		public void HostConnect(string Ip, int Port)
+		{
+			mCore.CommunicatorClientStartConnect(Ip, Port);
+		}
+		public void HostDisconnect()
+		{
+			mCore.CommunicatorClientStopConnect();
+		}
+		public void VehicleSimulatorStateReportSystemStart()
+		{
+			mCore.VehicleStateReporterStart();
+		}
+		public void VehicleSimulatorStateReportSystemStop()
+		{
+			mCore.VehicleStateReporterStop();
+		}
+		public void VehicleSimulatorStartMove(IEnumerable<IPoint2D> Path)
+		{
+			mCore.VehicleSimulatorInfoStartMove(Path);
+		}
+		public void VehicleSimulatorStopMove()
+		{
+			mCore.VehicleSimulatorInfoStopMove();
+		}
 
 		private void Constructor_VehicleManagerProcess()
 		{
+			UnsubscribeEvent_VehicleSimulatorProcess(mCore);
 			mCore = new Base.VehicleSimulatorProcess();
-			mCore.CommunicatorClientStartConnect("127.0.0.1", 8000);
-			//SubscribeEvent_VehicleSimulatorProcess(mCore);
+			SubscribeEvent_VehicleSimulatorProcess(mCore);
 
-			List<IPoint2D> tmp = new List<IPoint2D>();
-			tmp.Add(TrafficControlTest.Library.Library.GenerateIPoint2D(5000, 5000));
-			tmp.Add(TrafficControlTest.Library.Library.GenerateIPoint2D(-5000, 2123));
-			mCore.VehicleSimulatorInfoStartMove(tmp);
-
-			Task.Run(() => {
-				Thread.Sleep(5000);
-				List<IPoint2D> tmp2 = new List<IPoint2D>();
-				tmp2.Add(TrafficControlTest.Library.Library.GenerateIPoint2D(9000, 1000));
-				tmp2.Add(TrafficControlTest.Library.Library.GenerateIPoint2D(-5000, 2123));
-				tmp2.Add(TrafficControlTest.Library.Library.GenerateIPoint2D(-9974, -9231));
-				mCore.VehicleSimulatorInfoStartMove(tmp2);
-			});
 		}
 		private void Destructor_VehicleManagerProcess()
 		{
-			//UnsubscribeEvent_VehicleSimulatorProcess(mCore);
+			UnsubscribeEvent_VehicleSimulatorProcess(mCore);
 			mCore = null;
+		}
+		private void SubscribeEvent_VehicleSimulatorProcess(Base.VehicleSimulatorProcess VehicleSimulatorProcess)
+		{
+			if (VehicleSimulatorProcess != null)
+			{
+				VehicleSimulatorProcess.VehicleSimulatorInfoStateUpdated += HandleEvent_VehicleSimulatorProcessVehicleSimulatorInfoStateUpdated;
+				VehicleSimulatorProcess.CommunicatorClientSystemStarted += HandleEvent_VehicleSimulatorProcessCommunicatorClientSystemStarted;
+				VehicleSimulatorProcess.CommunicatorClientSystemStopped += HandleEvent_VehicleSimulatorProcessCommunicatorClientSystemStopped;
+				VehicleSimulatorProcess.CommunicatorClientConnectStateChanged += HandleEvent_VehicleSimulatorProcessCommunicatorClientConnectStateChanged;
+				VehicleSimulatorProcess.CommunicatorClientSentSerializableData += HandleEvent_VehicleSimulatorProcessCommunicatorClientSentSerializableData;
+				VehicleSimulatorProcess.CommunicatorClientReceivedSerializableData += HandleEvent_VehicleSimulatorProcessCommunicatorClientReceivedSerializableData;
+				VehicleSimulatorProcess.VehicleStateReporterSystemStarted += HandleEvent_VehicleSimulatorProcessVehicleStateReporterSystemStarted;
+				VehicleSimulatorProcess.VehicleStateReporterSystemStopped += HandleEvent_VehicleSimulatorProcessVehicleStateReporterSystemStopped;
+			}
+		}
+		private void UnsubscribeEvent_VehicleSimulatorProcess(Base.VehicleSimulatorProcess VehicleSimulatorProcess)
+		{
+			if (VehicleSimulatorProcess != null)
+			{
+				VehicleSimulatorProcess.VehicleSimulatorInfoStateUpdated -= HandleEvent_VehicleSimulatorProcessVehicleSimulatorInfoStateUpdated;
+				VehicleSimulatorProcess.CommunicatorClientSystemStarted -= HandleEvent_VehicleSimulatorProcessCommunicatorClientSystemStarted;
+				VehicleSimulatorProcess.CommunicatorClientSystemStopped -= HandleEvent_VehicleSimulatorProcessCommunicatorClientSystemStopped;
+				VehicleSimulatorProcess.CommunicatorClientConnectStateChanged -= HandleEvent_VehicleSimulatorProcessCommunicatorClientConnectStateChanged;
+				VehicleSimulatorProcess.CommunicatorClientSentSerializableData -= HandleEvent_VehicleSimulatorProcessCommunicatorClientSentSerializableData;
+				VehicleSimulatorProcess.CommunicatorClientReceivedSerializableData -= HandleEvent_VehicleSimulatorProcessCommunicatorClientReceivedSerializableData;
+				VehicleSimulatorProcess.VehicleStateReporterSystemStarted -= HandleEvent_VehicleSimulatorProcessVehicleStateReporterSystemStarted;
+				VehicleSimulatorProcess.VehicleStateReporterSystemStopped -= HandleEvent_VehicleSimulatorProcessVehicleStateReporterSystemStopped;
+			}
+		}
+		private void HandleEvent_VehicleSimulatorProcessVehicleSimulatorInfoStateUpdated(DateTime OccurTime, string Name, IVehicleSimulatorInfo VehicleSimulatorInfo)
+		{
+
+		}
+		private void HandleEvent_VehicleSimulatorProcessCommunicatorClientSystemStarted(DateTime OccurTime)
+		{
+
+		}
+		private void HandleEvent_VehicleSimulatorProcessCommunicatorClientSystemStopped(DateTime OccurTime)
+		{
+
+		}
+		private void HandleEvent_VehicleSimulatorProcessCommunicatorClientConnectStateChanged(DateTime OccurTime, string IpPort, ConnectState NewState)
+		{
+			UpdateGui_ToolStripItemBackColor(statusStrip1, statusLabelHostConnectState, NewState == ConnectState.Connected ? Color.LightGreen : Color.LightPink);
+			UpdateGui_ToolStripItemBackColor(menuStrip1, menuHostConnection, NewState == ConnectState.Connected ? Color.LightGreen : Color.LightPink);
+		}
+		private void HandleEvent_VehicleSimulatorProcessCommunicatorClientSentSerializableData(DateTime OccurTime, string IpPort, object Data)
+		{
+
+		}
+		private void HandleEvent_VehicleSimulatorProcessCommunicatorClientReceivedSerializableData(DateTime OccurTime, string IpPort, object Data)
+		{
+
+		}
+		private void HandleEvent_VehicleSimulatorProcessVehicleStateReporterSystemStarted(DateTime OccurTime)
+		{
+
+		}
+		private void HandleEvent_VehicleSimulatorProcessVehicleStateReporterSystemStopped(DateTime OccurTime)
+		{
+
+		}
+		#endregion
+
+		private void menuHostConnect_Click(object sender, EventArgs e)
+		{
+			if (GetIpPort(menuHostIpPort.Text, out string Ip, out int Port))
+			{
+				mCore.CommunicatorClientStartConnect(Ip, Port);
+			}
+		}
+		private void btnVehicleSimulatorStartMove_Click(object sender, EventArgs e)
+		{
+			if (GetPath(txtVehicleSimulatorPath.Text, out IEnumerable<IPoint2D> Path))
+			{
+				mCore.VehicleSimulatorInfoStartMove(Path);
+			}
+		}
+		private void btnVehicleSimulatorStopMove_Click(object sender, EventArgs e)
+		{
+			mCore.VehicleSimulatorInfoStopMove();
 		}
 	}
 }
