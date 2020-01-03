fb5dd90500fc95d716e9035f9ecf88f6543366ab
frank <frankpan@castec.com.tw>
2020-01-03 16:22:01 +0800

    - IImportantEventRecorder 新增註解

--- TrafficControlTest/Module/General/Interface/IImportantEventRecorder.cs
+++ TrafficControlTest/Module/General/Interface/IImportantEventRecorder.cs
@@ -11,9 +11,9 @@ namespace TrafficControlTest.Module.General.Interface
 {
 	/// <summary>
 	/// - Reference: IEventRecorder, IVehicleInfoManager, IMissionStateManager
-	/// - 每當 IVehicleInfoManager 發生 ItemAdded, ItemRemoved, ItemUpdated 事件時，使用 IEventRecorder 將資料記錄至資料庫
+	/// - 每當 IVehicleInfoManager 發生 ItemAdded, ItemRemoved, ItemUpdated 事件時，使用 IEventRecorder 將資料記錄至資料庫(儲存當前車子狀態，以供網頁即時顯示車子狀態)
 	/// - 每當 IMissionStateManager 發生 ItemAdded, ItemUpdated 事件時，使用 IEventRecorder 將資料記錄至資料庫
-	/// - 定時從 IVehicleInfoManager 拿取所有車的狀態資訊並使用 IEventRecorder 記錄至資料庫
+	/// - 定時從 IVehicleInfoManager 拿取所有車的狀態資訊並使用 IEventRecorder 記錄至資料庫(儲存歷史車子狀態，以供未來重現、查詢問題時使用)
 	/// </summary>
 	public interface IImportantEventRecorder
 	{
