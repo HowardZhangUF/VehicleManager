f669966c6b1bf79bafe8d951a553c14650492e06
frank <frankpan@castec.com.tw>
2020-03-10 11:49:57 +0800

    - 新增 SystemWithLoopTask 類別

---
+++ TrafficControlTest/Module/General/Implement/SystemWithLoopTask.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TrafficControlTest.Module.General.Interface;
+using static TrafficControlTest.Library.EventHandlerLibrary;
+
+namespace TrafficControlTest.Module.General.Implement
+{
+    public abstract class SystemWithLoopTask : ISystemWithLoopTask
+    {
+        public event EventHandlerDateTime SystemStarted;
+        public event EventHandlerDateTime SystemStopped;
+
+        public bool mIsExecuting 
+        {
+            get
+            {
+                return _IsExecuting;
+            }
+            private set
+            {
+                _IsExecuting = value;
+                if (_IsExecuting) RaiseEvent_SystemStarted();
+                else RaiseEvent_SystemStopped();
+            }
+        }
+        public int mTimePeriod { get; set; } = 500;
+
+        private Thread mThdLoop = null;
+        private bool[] mThdLoopExitFlag = null;
+        private bool _IsExecuting = false;
+
+        public void Start()
+        {
+            InitializeThread();
+        }
+        public void Stop()
+        {
+            DestroyThread();
+        }
+        public abstract void Task();
+
+        protected virtual void RaiseEvent_SystemStarted(bool Sync = true)
+        {
+            if (Sync)
+            {
+                SystemStarted?.Invoke(DateTime.Now);
+            }
+            else
+            {
+				System.Threading.Tasks.Task.Run(() => { SystemStarted?.Invoke(DateTime.Now); });
+            }
+        }
+        protected virtual void RaiseEvent_SystemStopped(bool Sync = true)
+        {
+            if (Sync)
+            {
+                SystemStopped?.Invoke(DateTime.Now);
+            }
+            else
+            {
+				System.Threading.Tasks.Task.Run(() => { SystemStopped?.Invoke(DateTime.Now); });
+            }
+        }
+
+        private void InitializeThread()
+        {
+            mThdLoopExitFlag = new bool[] { false };
+            mThdLoop = new Thread(() => LoopTask(mThdLoopExitFlag));
+            mThdLoop.IsBackground = true;
+            mThdLoop.Start();
+        }
+        private void DestroyThread()
+        {
+            if (mThdLoop != null)
+            {
+                if (mThdLoop.IsAlive)
+                {
+                    mThdLoopExitFlag[0] = true;
+                }
+                mThdLoop = null;
+            }
+        }
+        private void LoopTask(bool[] ExitFlag)
+        {
+            try
+            {
+                mIsExecuting = true;
+                while (!ExitFlag[0])
+                {
+                    Task();
+                    Thread.Sleep(mTimePeriod);
+                }
+            }
+            finally
+            {
+                mIsExecuting = false;
+            }
+        }
+    }
+}
---
+++ TrafficControlTest/Module/General/Interface/ISystemWithLoopTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TrafficControlTest.Library.EventHandlerLibrary;
+
+namespace TrafficControlTest.Module.General.Interface
+{
+    public interface ISystemWithLoopTask
+    {
+        event EventHandlerDateTime SystemStarted;
+        event EventHandlerDateTime SystemStopped;
+
+        bool mIsExecuting { get; }
+        int mTimePeriod { get; set; }
+
+        void Start();
+        void Stop();
+		/// <summary>此方法將被迴圈執行</summary>
+        void Task();
+    }
+}
