2b84c4597f7ce0cf7e0050839f69cd2f39963049
frank <frankpan@castec.com.tw>
2020-06-09 10:51:06 +0800

    - Mission 修改 ParametersString 的計算方式

--- TrafficControlTest/Module/Mission/Mission.cs
+++ TrafficControlTest/Module/Mission/Mission.cs
@@ -9,7 +9,26 @@ namespace TrafficControlTest.Module.Mission
 		public int mPriority { get; private set; }
 		public string mVehicleId { get; private set; }
 		public string[] mParameters { get; private set; }
-		public string mParametersString { get { return mParameters == null ? string.Empty : string.Join(",", mParameters); } }
+		public string mParametersString
+		{
+			get
+			{
+				if (mParameters == null)
+				{
+					return string.Empty;
+				}
+				else if (mParameters.Length == 1)
+				{
+					// 參數只有一個，代表是 Goal 點
+					return mParameters[0];
+				}
+				else
+				{
+					// 參數有多個，可能為 X + Y 或是 X + Y + Toward
+					return $"({string.Join(",", mParameters)})";
+				}
+			}
+		}
 
 		public Mission(MissionType MissionType, string MissionId, int Priority, string VehicleId, string[] Parameters)
 		{
