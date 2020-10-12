36a00d6c2b5c0c62d165910879f1e7eb6d3e99d4
frank <frankpan@castec.com.tw>
2020-10-12 10:47:52 +0800

    - 新增 IAutomaticDoorControl 與 IAutomaticDoorControlManager 及其實作

---
+++ TrafficControlTest/Module/AutomaticDoor/AutomaticDoorControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficControlTest.Module.General;
+
+namespace TrafficControlTest.Module.AutomaticDoor
+{
+	public class AutomaticDoorControl : IAutomaticDoorControl
+	{
+		public event EventHandler<StatusUpdatedEventArgs> StatusUpdated;
+
+		public string mName { get; private set; } = string.Empty;
+		public string mAutomaticDoorName { get; private set; } = string.Empty;
+		public AutomaticDoorControlCommand mCommand { get; private set; } = AutomaticDoorControlCommand.None;
+		public string mCause { get; private set; } = string.Empty;
+		public AutomaticDoorControlCommandSendState mSendState { get; private set; } = AutomaticDoorControlCommandSendState.Unsend;
+		public DateTime mLastUpdated { get; private set; } = DateTime.Now;
+
+		public void Set(string AutomaticDoorName, AutomaticDoorControlCommand Command, string Cause)
+		{
+			mName = $"ControlFor{AutomaticDoorName}";
+			mAutomaticDoorName = AutomaticDoorName;
+			mCommand = Command;
+			mCause = Cause;
+			mLastUpdated = DateTime.Now;
+			RaiseEvent_StatusUpdated("Name,AutomaticDoorName,Command,Cause");
+		}
+		public void UpdateSendState(AutomaticDoorControlCommandSendState SendState)
+		{
+			mSendState = SendState;
+			mLastUpdated = DateTime.Now;
+			RaiseEvent_StatusUpdated("SendState");
+		}
+
+		protected virtual void RaiseEvent_StatusUpdated(string StatusName, bool Sync = true)
+		{
+			if (Sync)
+			{
+				StatusUpdated?.Invoke(this, new StatusUpdatedEventArgs(DateTime.Now, mName, StatusName));
+			}
+			else
+			{
+				Task.Run(() => { StatusUpdated?.Invoke(this, new StatusUpdatedEventArgs(DateTime.Now, mName, StatusName)); });
+			}
+		}
+	}
+}
---
+++ TrafficControlTest/Module/AutomaticDoor/AutomaticDoorControlManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficControlTest.Module.General;
+
+namespace TrafficControlTest.Module.AutomaticDoor
+{
+	public class AutomaticDoorControlManager : ItemManager<IAutomaticDoorControl>, IAutomaticDoorControlManager
+	{
+		public void UpdateSendState(string Name, AutomaticDoorControlCommandSendState SendState)
+		{
+			if (IsExist(Name))
+			{
+				mItems[Name].UpdateSendState(SendState);
+			}
+		}
+	}
+}
