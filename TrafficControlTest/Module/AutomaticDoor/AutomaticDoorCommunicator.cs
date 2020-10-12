ce12ccd3881ec2a3f7cc8f79c2dfaed8f028cb63
frank <frankpan@castec.com.tw>
2020-10-12 15:22:36 +0800

    - 新增 IAutomaticDoorCommunicator 及其實作

---
+++ TrafficControlTest/Module/AutomaticDoor/AutomaticDoorCommunicator.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficControlTest.Module.Communication;
+using TrafficControlTest.Module.General;
+
+namespace TrafficControlTest.Module.AutomaticDoor
+{
+	public class AutomaticDoorCommunicator : SystemWithLoopTask, IAutomaticDoorCommunicator
+	{
+		public event EventHandler<ClientAddedEventArgs> ClientAdded;
+		public event EventHandler<ClientRemovedEventArgs> ClientRemoved;
+		public event EventHandler<RemoteConnectStateChangedEventArgs> RemoteConnectStateChanged;
+		public event EventHandler<SentDataEventArgs> SentData;
+		public event EventHandler<ReceivedDataEventArgs> ReceivedData;
+
+		private Dictionary<string, ICommunicatorClientUsingString> mClients = new Dictionary<string, ICommunicatorClientUsingString>();
+		private bool mAutoConnect = false;
+		
+		public List<string> GetClientList()
+		{
+			if (mClients.Any())
+			{
+				return mClients.Keys.ToList();
+			}
+			else
+			{
+				return null;
+			}
+		}
+		public bool IsConnected(string IpPort)
+		{
+			if (mClients.Keys.Contains(IpPort))
+			{
+				return mClients[IpPort].mIsConnected;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		public void Add(string IpPort)
+		{
+			if (!mClients.Keys.Contains(IpPort))
+			{
+				mClients.Add(IpPort, new CommunicatorClientUsingString());
+				mClients[IpPort].ConnectStateChanged += HandleEvent_ICommunicatorClientUsingStringConnectStateChanged;
+				mClients[IpPort].SentString += HandleEvent_ICommunicatorClientUsingStringSentString;
+				mClients[IpPort].ReceivedString += HandleEvent_ICommunicatorClientUsingStringReceivedString;
+				RaiseEvent_ClientAdded(IpPort);
+			}
+		}
+		public void Remove(string IpPort)
+		{
+			if (mClients.Keys.Contains(IpPort))
+			{
+				mClients[IpPort].ConnectStateChanged -= HandleEvent_ICommunicatorClientUsingStringConnectStateChanged;
+				mClients[IpPort].SentString -= HandleEvent_ICommunicatorClientUsingStringSentString;
+				mClients[IpPort].ReceivedString -= HandleEvent_ICommunicatorClientUsingStringReceivedString;
+				mClients.Remove(IpPort);
+				RaiseEvent_ClientRemoved(IpPort);
+			}
+		}
+		public void RemoveAll()
+		{
+			while (mClients.Any())
+			{
+				Remove(mClients.Keys.First());
+			}
+		}
+		public void Connect(string IpPort)
+		{
+			if (mClients.Keys.Contains(IpPort) && !mClients[IpPort].mIsConnected)
+			{
+				mClients[IpPort].Connect(IpPort);
+			}
+		}
+		public void Disconnect(string IpPort)
+		{
+			if (mClients.Keys.Contains(IpPort) && mClients[IpPort].mIsConnected)
+			{
+				mClients[IpPort].Disconnect();
+			}
+		}
+		public void SendData(string IpPort, string Data)
+		{
+			if (mClients.Keys.Contains(IpPort) && mClients[IpPort].mIsConnected)
+			{
+				mClients[IpPort].Send(Data);
+			}
+		}
+		public override string GetConfig(string ConfigName)
+		{
+			switch (ConfigName)
+			{
+				case "TimePeriod":
+					return mTimePeriod.ToString();
+				case "AutoConnect":
+					return mAutoConnect.ToString();
+				default:
+					return null;
+			}
+		}
+		public override void SetConfig(string ConfigName, string NewValue)
+		{
+			switch (ConfigName)
+			{
+				case "TimePeriod":
+					mTimePeriod = int.Parse(NewValue);
+					RaiseEvent_ConfigUpdated(ConfigName, NewValue);
+					break;
+				case "AutoConnect":
+					mAutoConnect = bool.Parse(NewValue);
+					RaiseEvent_ConfigUpdated(ConfigName, NewValue);
+					break;
+				default:
+					break;
+			}
+		}
+		public override void Task()
+		{
+			Subtask_AutoConnect();
+		}
+		
+		protected virtual void RaiseEvent_ClientAdded(string IpPort, bool Sync = true)
+		{
+			if (Sync)
+			{
+				ClientAdded?.Invoke(this, new ClientAddedEventArgs(DateTime.Now, IpPort));
+			}
+			else
+			{
+				System.Threading.Tasks.Task.Run(() => { ClientAdded?.Invoke(this, new ClientAddedEventArgs(DateTime.Now, IpPort)); });
+			}
+		}
+		protected virtual void RaiseEvent_ClientRemoved(string IpPort, bool Sync = true)
+		{
+			if (Sync)
+			{
+				ClientRemoved?.Invoke(this, new ClientRemovedEventArgs(DateTime.Now, IpPort));
+			}
+			else
+			{
+				System.Threading.Tasks.Task.Run(() => { ClientRemoved?.Invoke(this, new ClientRemovedEventArgs(DateTime.Now, IpPort)); });
+			}
+		}
+		protected virtual void RaiseEvent_RemoteConnectStateChanged(string IpPort, bool Connected, bool Sync = true)
+		{
+			if (Sync)
+			{
+				RemoteConnectStateChanged?.Invoke(this, new RemoteConnectStateChangedEventArgs(DateTime.Now, IpPort, Connected));
+			}
+			else
+			{
+				System.Threading.Tasks.Task.Run(() => { RemoteConnectStateChanged?.Invoke(this, new RemoteConnectStateChangedEventArgs(DateTime.Now, IpPort, Connected)); });
+			}
+		}
+		protected virtual void RaiseEvent_SentData(string IpPort, string Data, bool Sync = true)
+		{
+			if (Sync)
+			{
+				SentData?.Invoke(this, new SentDataEventArgs(DateTime.Now, IpPort, Data));
+			}
+			else
+			{
+				System.Threading.Tasks.Task.Run(() => { SentData?.Invoke(this, new SentDataEventArgs(DateTime.Now, IpPort, Data)); });
+			}
+		}
+		protected virtual void RaiseEvent_ReceivedData(string IpPort, string Data, bool Sync = true)
+		{
+			if (Sync)
+			{
+				ReceivedData?.Invoke(this, new ReceivedDataEventArgs(DateTime.Now, IpPort, Data));
+			}
+			else
+			{
+				System.Threading.Tasks.Task.Run(() => { ReceivedData?.Invoke(this, new ReceivedDataEventArgs(DateTime.Now, IpPort, Data)); });
+			}
+		}
+
+		private void HandleEvent_ICommunicatorClientUsingStringConnectStateChanged(object sender, ConnectStateChangedEventArgs e)
+		{
+			RaiseEvent_RemoteConnectStateChanged(e.IpPort, e.Connected);
+		}
+		private void HandleEvent_ICommunicatorClientUsingStringSentString(object sender, SentStringEventArgs e)
+		{
+			RaiseEvent_SentData(e.IpPort, e.Data);
+		}
+		private void HandleEvent_ICommunicatorClientUsingStringReceivedString(object sender, ReceivedStringEventArgs e)
+		{
+			RaiseEvent_ReceivedData(e.IpPort, e.Data);
+		}
+		private void Subtask_AutoConnect()
+		{
+			if (mAutoConnect)
+			{
+				if (mClients.Any())
+				{
+					foreach (string ipPort in mClients.Keys)
+					{
+						if (!mClients[ipPort].mIsConnected)
+						{
+							mClients[ipPort].Connect(ipPort);
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ TrafficControlTest/Module/AutomaticDoor/IAutomaticDoorCommunicator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficControlTest.Module.General;
+
+namespace TrafficControlTest.Module.AutomaticDoor
+{
+	public interface IAutomaticDoorCommunicator : ISystemWithLoopTask
+	{
+		event EventHandler<ClientAddedEventArgs> ClientAdded;
+		event EventHandler<ClientRemovedEventArgs> ClientRemoved;
+		event EventHandler<RemoteConnectStateChangedEventArgs> RemoteConnectStateChanged;
+		event EventHandler<SentDataEventArgs> SentData;
+		event EventHandler<ReceivedDataEventArgs> ReceivedData;
+
+		List<string> GetClientList();
+		bool IsConnected(string IpPort);
+		void Add(string IpPort);
+		void Remove(string IpPort);
+		void Connect(string IpPort);
+		void Disconnect(string IpPort);
+		void RemoveAll();
+		void SendData(string IpPort, string Data);
+	}
+
+	public class ClientAddedEventArgs : EventArgs
+	{
+		public DateTime OccurTime { get; private set; }
+		public string IpPort { get; private set; }
+
+		public ClientAddedEventArgs(DateTime OccurTime, string IpPort)
+		{
+			this.OccurTime = OccurTime;
+			this.IpPort = IpPort;
+		}
+	}
+
+	public class ClientRemovedEventArgs : EventArgs
+	{
+		public DateTime OccurTime { get; private set; }
+		public string IpPort { get; private set; }
+
+		public ClientRemovedEventArgs(DateTime OccurTime, string IpPort)
+		{
+			this.OccurTime = OccurTime;
+			this.IpPort = IpPort;
+		}
+	}
+
+	public class RemoteConnectStateChangedEventArgs : EventArgs
+	{
+		public DateTime OccurTime { get; private set; }
+		public string IpPort { get; private set; }
+		public bool Connected { get; private set; }
+
+		public RemoteConnectStateChangedEventArgs(DateTime OccurTime, string IpPort, bool Connected)
+		{
+			this.OccurTime = OccurTime;
+			this.IpPort = IpPort;
+			this.Connected = Connected;
+		}
+	}
+
+	public class SentDataEventArgs : EventArgs
+	{
+		public DateTime OccurTime { get; private set; }
+		public string IpPort { get; private set; }
+		public string Data { get; private set; }
+
+		public SentDataEventArgs(DateTime OccurTime, string IpPort, string Data)
+		{
+			this.OccurTime = OccurTime;
+			this.IpPort = IpPort;
+			this.Data = Data;
+		}
+	}
+
+	public class ReceivedDataEventArgs : EventArgs
+	{
+		public DateTime OccurTime { get; private set; }
+		public string IpPort { get; private set; }
+		public string Data { get; private set; }
+
+		public ReceivedDataEventArgs(DateTime OccurTime, string IpPort, string Data)
+		{
+			this.OccurTime = OccurTime;
+			this.IpPort = IpPort;
+			this.Data = Data;
+		}
+	}
+}
