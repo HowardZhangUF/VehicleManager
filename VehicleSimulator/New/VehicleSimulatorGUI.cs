5ed2a2c64c7706e6f61bf6ab5e142d2cb16e7f37
frankpan <frankpan@castec.com.tw>
2022-02-18 16:04:25 +0800

    - VehicleSimulator 移除對 TrafficControlTest 的參考

--- VehicleSimulator/New/VehicleSimulatorGUI.cs
+++ VehicleSimulator/New/VehicleSimulatorGUI.cs
@@ -1,19 +1,13 @@
 ﻿using LibraryForVM;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using TrafficControlTest.Process;
 
 namespace VehicleSimulator.New
 {
-    public enum Language
+	public enum Language
     {
         EnUs,
         ZhTw
