04198bec5fb5aee2ecaf90e3a9a1a32d3e6e60ac
frankpan <frankpan@castec.com.tw>
2022-02-18 15:25:31 +0800

    - 移動部分檔案

--- TrafficControlTest/Module/CollisionEvent/IntMath.cs
+++ TrafficControlTest/Module/CollisionEvent/IntMath.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrafficControlTest.Library
+namespace TrafficControlTest.Module.CollisionEvent
 {
 	[Serializable]
 	public class IntMath : TypeMath<int>
